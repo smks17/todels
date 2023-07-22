026a9de272e95299dd21b4ebd514a13672d6ceaa
Mahdi <esmokes17@gmail.com>
2023-07-22 22:27:03 +0330

    Implement multiply outputs for AE

--- todels/autoencoder.py
+++ todels/autoencoder.py
@@ -1,4 +1,6 @@
-from typing import List, Tuple
+from __future__ import annotations
+
+from typing import List, Tuple, Iterable
 
 import torch
 import torch.nn as nn
@@ -6,51 +8,92 @@ import torch.nn as nn
 
 class AE(nn.Module):
     """vanilla Autoencoder"""
-    def __init__(self, encoder: nn.Module, decoder: nn.Module):
+    def __init__(self,
+                 encoder: nn.Module,
+                 decoder: nn.Module | Iterable[nn.Module]):
         super(AE, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
 
-    def forward(self, x):
-        return self.decoder(self.encoder(x))
+    def encode(self, x):
+        return self.encoder(x)
 
+    def decode(self, z) -> torch.Tensor | list[torch.Tensor]:
+        if isinstance(self.decoder, Iterable):
+            outputs = []
+            for d in self.decoder:
+                outputs.append(d(z))
+            return outputs
+        else:
+            return self.decoder(z)
 
-class MultiAE(nn.Module):
-    """Have one input encoder and one or more output from decoders"""
-    def __init__(self, encoder: nn.Module, decoders: List[nn.Module]):
-        super(AE, self).__init__()
-        self.encoder = encoder
-        self.decoders = decoders
-
-    def forward(self, x) -> List[torch.Tensor]:
-        outputs = []
-        z = self.encoder(x)
-        for decoder in self.decoders:
-            outputs.append(decoder(z))
-        return outputs
+    def forward(self, x) -> torch.Tensor | list[torch.Tensor]:
+        return self.decode(self.encode(x))
 
 
 class VAE(AE):
-    def __init__(self, encoder: nn.Module, decoder: nn.Module, latent_size: int):
+    def __init__(self,
+                 encoder: nn.Module,
+                 decoder: nn.Module | Iterable[nn.Module],
+                 latent_size: int):
         super(VAE, self).__init__(encoder, decoder)
-        self.mean_net = nn.LazyLinear(latent_size)
-        self.var_net = nn.LazyLinear(latent_size)
+        if isinstance(self.decoder, Iterable):
+            mean_net = []
+            var_net = []
+            for i in range(len(self.decoder)):
+                mean_net.append(nn.LazyLinear(latent_size))
+                var_net.append(nn.LazyLinear(latent_size))
+            self.mean_net = mean_net
+            self.var_net = var_net
+        else:
+            self.mean_net = nn.LazyLinear(latent_size)
+            self.var_net = nn.LazyLinear(latent_size)
 
-    def encode(self, x) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def encode(self, x) -> Tuple[torch.Tensor, torch.Tensor | list[torch.Tensor], torch.Tensor | list[torch.Tensor]]:
         z = self.encoder(x)
-        return self.mean_net(z), self.var_net(z)
+        return z, self.mean(z), self.var(z)
 
-    def decode(self, x):
-        return self.decoder(x)
+    def mean(self, z) -> torch.Tensor | list[torch.Tensor]:
+        if isinstance(self.mean_net, Iterable):
+            outputs = []
+            for m in self.mean_net:
+                outputs.append(m(z))
+            return outputs
+        else:
+            return self.mean_net(z)
 
-    def get_representation(self, var, mean) -> torch.Tensor:
-        std = torch.exp(var * 0.5)
-        epsilon = torch.randn_like(std)
-        return mean + (epsilon * var)
+    def var(self, z) -> torch.Tensor | list[torch.Tensor]:
+        if isinstance(self.var_net, Iterable):
+            outputs = []
+            for m in self.var_net:
+                outputs.append(m(z))
+            return outputs
+        else:
+            return self.var_net(z)
+
+    def get_representation(self, var, mean) -> torch.Tensor | List[torch.Tensor]:
+        if isinstance(self.var_net, Iterable):
+            outputs = []
+            for i in range(len(self.var_net)):
+                std = torch.exp(var[i] * 0.5)
+                epsilon = torch.randn_like(std)
+                outputs.append(mean[i] + (epsilon * var[i]))
+            return outputs
+        else:
+            std = torch.exp(var * 0.5)
+            epsilon = torch.randn_like(std)
+            return mean + (epsilon * var)
 
     def forward(self, x) -> torch.Tensor:
-        mean, var = self.encode(x)
-        return self.decoder(self.get_representation(var, mean)), mean, var
+        z, mean, var = self.encode(x)
+        if isinstance(self.decoder, Iterable):
+            rz = self.get_representation(var, mean)
+            outputs = []
+            for i in range(len(rz)):
+                outputs.append(self.decode(rz[i]))
+            return outputs, mean, var
+        else:
+            return self.decode(self.get_representation(mean, var)), mean, var
 
     @staticmethod
     def cal_loss(x, recons_loss, mean, var, kld_weight=0.1):
@@ -60,7 +103,9 @@ class VAE(AE):
 
 
 class CAE(AE):
-    def __init__(self, encoder: nn.Module, decoder: nn.Module):
+    def __init__(self,
+                 encoder: nn.Module,
+                 decoder: nn.Module | Iterable[nn.Module]):
         super(CAE, self).__init__(encoder, decoder)
         self.sigmoid = nn.Sigmoid()
         
